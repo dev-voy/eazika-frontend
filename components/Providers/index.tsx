94244830c1a52bc57b7d03dbe6865b929968df28
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-09-30 11:19:56 +0530

    Refactor user management: update role constants, enhance token handling, and improve user state management

--- components/Providers/index.tsx
+++ components/Providers/index.tsx
@@ -4,8 +4,25 @@ import * as React from "react";
 import { Toaster } from "@/components/ui/sonner";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "@/store";
+import Cookies from "js-cookie";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  React.useEffect(() => {
+    const { accessToken, refreshToken } = Cookies.get();
+    const user = localStorage.getItem("user");
+    if (accessToken) {
+      store.dispatch({
+        type: "auth/setToken",
+        payload: { accessToken, refreshToken },
+      });
+    }
+    if (user) {
+      store.dispatch({
+        type: "user/setUser",
+        payload: JSON.parse(user || "{}"),
+      });
+    }
+  });
   return (
     <>
       <ReduxProvider store={store}>
